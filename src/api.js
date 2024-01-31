407dae50cca3c08ba0c7db2613185447f2efb482
zntb <zenetibi@yahoo.com>
2024-01-31 15:47:40 +0200

    Building city search component

---
+++ src/api.js
@@ -0,0 +1,11 @@
+const rapidApiKey = process.env.REACT_APP_X_RAPIDAPI_KEY;
+
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': rapidApiKey,
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+  },
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
